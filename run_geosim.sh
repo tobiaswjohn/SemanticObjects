a78440d2163ecc7038725e91fd8f0a543fbabe92
Tobias John <tobiajoh@ifi.uio.no>
2024-04-09 15:26:42 +0200

    modify execution script

--- run_geosim.sh
+++ run_geosim.sh
@@ -5,6 +5,8 @@
 # e.g. examples/Geological/total_mini.ttl
 
 Ontology=$1
+Program=${2:-"examples/Geological/simulate_onto.smol"}
 
 
-java -jar build/libs/smol.jar -i examples/Geological/simulate_onto.smol -v -e -b $Ontology  -p UFRGS1=https://www.inf.ufrgs.br/bdi/ontologies/geocoreontology#UFRGS -p obo=http://purl.obolibrary.org/obo/ -d http://www.semanticweb.org/quy/ontologies/2023/2/untitled-ontology-38#
+
+java -jar build/libs/smol.jar -i $Program -v -e -b $Ontology  -p UFRGS1=https://www.inf.ufrgs.br/bdi/ontologies/geocoreontology#UFRGS -p obo=http://purl.obolibrary.org/obo/ -d http://www.semanticweb.org/quy/ontologies/2023/2/untitled-ontology-38#
